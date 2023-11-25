3cb8a96a7b8371b5b35002b8eb0f004ff3c0af15
Rohith <rohithremesan507@gmail.com>
2023-11-25 19:06:57 +0530

    login bug fix

--- app/src/main/java/com/example/voyager/view/fragment/LoginFragment.kt
+++ app/src/main/java/com/example/voyager/view/fragment/LoginFragment.kt
@@ -4,6 +4,7 @@ import android.os.Build
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -33,6 +34,7 @@ private val viewModel:UserViewModel by viewModels()
        binding= FragmentLoginBinding.inflate(layoutInflater,container,false)
         binding.lifecycleOwner=viewLifecycleOwner
         binding.clickHelper=this
+        binding.viewModel=viewModel
         return binding.root
     }
 
